458a07d7cb5d43515e0b63a78c87df49c4717ab7
ze <antoniojosemribeiro@gmail.com>
2015-10-23 19:01:15 +0100

    improved ti_disappear.py script among other things changed '-' in filenames to '_' and lambdas are written with three numbers

--- TI/ti_disappear.py
+++ TI/ti_disappear.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-15 -*-
 
+
 import sys
 import numpy as np
 
@@ -20,7 +21,7 @@ bPrmtopFile = 'solvent.prmtop'
 bInpcrdFile = 'solvent.inpcrd'
 
 
-#### Input de Minimização ####
+#### Input de MinimizaÃ§Ã£o ####
 def writeMin(name,NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA):
   minFile = \
   """Minimizacao
@@ -41,7 +42,7 @@ def writeMin(name,NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA):
     clambda = %s
    &end""" % (NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA)
   open('min' + name, 'w').write(minFile)
-#### Input de Equilibração ###
+#### Input de EquilibraÃ§Ã£o ###
 def writeEqui(name,NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA):
   equiFile =\
   """Equilibracao
@@ -73,7 +74,7 @@ def writeEqui(name,NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA):
   &end""" % (NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA)
   open('equi' + name, 'w').write(equiFile)
 
-#### Input de Produção  ######
+#### Input de ProduÃ§Ã£o  ######
 def writeProd(name,NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA):
   prodFile = \
   """Producao
@@ -105,80 +106,77 @@ def writeProd(name,NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA):
   &end""" % (NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA)
   open('prod' + name, 'w').write(prodFile)
 #####      Group File   #####
-def writeGroup(passo,value,aPrmtopFile,aInpcrdFile,bPrmtopFile,bInpcrdFile):
+def writeGroup(step,lvalue,aPrmtopFile,aInpcrdFile,bPrmtopFile,bInpcrdFile):
   
-   if passo == '1':
-      ViRstFile = aInpcrdFile
-      VfRstFile = aInpcrdFile
-   elif passo == '2':
-      ViRstFile = aInpcrdFile
-      VfRstFile = bInpcrdFile    
+    if step == '1':
+        ViRstFile = aInpcrdFile
+        VfRstFile = aInpcrdFile
+    elif step == '2':
+        ViRstFile = aInpcrdFile
+        VfRstFile = bInpcrdFile    
     
-  if passo == '1':  #prmtop 
-    ViPrmtopFile = aPrmtopFile
-    VfPrmtopFile = aPrmtopFile
-  elif passo == '2':
-    ViPrmtopFile = aPrmtopFile
-    VfPrmtopFile = bPrmtopFile      
+    if step == '1':  #prmtop 
+        ViPrmtopFile = aPrmtopFile
+        VfPrmtopFile = aPrmtopFile
+    elif step == '2':
+        ViPrmtopFile = aPrmtopFile
+        VfPrmtopFile = bPrmtopFile      
+
+    step_lambda_str = '_{0}_{1:.3f}'.format(step, lvalue)
+  
+    groupMin = """-O -i min{0}_Vi.in -o min{0}_Vi.out -p {1} -c {2} -r min{0}_Vi.rst\n"""\
+               """-O -i min{0}_Vf.in -o min{0}_Vf.out -p {3} -c {4} -r min{0}_Vf.rst""".format(
+           step_lambda_str, ViPrmtopFile, ViRstFile, VfPrmtopFile, VfRstFile)
     
-
-  groupMin = \
-  """-O -i min-%s-%s-Vi.in -o min-%s-%s-Vi.out -p %s -c %s -r min-%s-%s-Vi.rst
--O -i min-%s-%s-Vf.in -o min-%s-%s-Vf.out -p %s -c %s -r min-%s-%s-Vf.rst""" \
-  %(passo,value,passo,value,ViPrmtopFile,ViRstFile,passo,value,\
-  passo,value,passo,value,VfPrmtopFile,VfRstFile,passo,value)
+    open('min{0}.groupfile'.format(step_lambda_str), 'w').write(groupMin)
   
-  open('min-%s-%s.groupfile' %(passo,value), 'w').write(groupMin)
-  
-  groupEqui = \
-  """-O -i equi-%s-%s-Vi.in -o equi-%s-%s-Vi.out -p %s -c min-%s-%s-Vi.rst -r equi-%s-%s-Vi.rst
--O -i equi-%s-%s-Vf.in -o equi-%s-%s-Vf.out -p %s -c min-%s-%s-Vf.rst -r equi-%s-%s-Vf.rst""" \
-  %(passo,value,passo,value,ViPrmtopFile,passo,value,passo,value,\
-  passo,value,passo,value,VfPrmtopFile,passo,value,passo,value)
-  
-  open('equi-%s-%s.groupfile' %(passo,value), 'w').write(groupEqui)
-  
-  groupProd = \
-  """-O -i prod-%s-%s-Vi.in -o prod-%s-%s-Vi.out -p %s -c equi-%s-%s-Vi.rst -r prod-%s-%s-Vi.rst
--O -i prod-%s-%s-Vf.in -o prod-%s-%s-Vf.out -p %s -c equi-%s-%s-Vf.rst -r prod-%s-%s-Vf.rst"""\
-  %(passo,value,passo,value,ViPrmtopFile,passo,value,passo,value,\
-  passo,value,passo,value,VfPrmtopFile,passo,value,passo,value)
-  
-  open('prod-%s-%s.groupfile' %(passo,value), 'w').write(groupProd)
-
-for passo in ['1','2']:
-  if passo == '1':
-    scmask_Vi = ''
-    crgmask_Vi = ''
-    scmask_Vf = ''
-    crgmask_Vf = aMask
-    ifsc = '0'
-    ntmin = '1'
-  elif passo == '2':
-    scmask_Vi = aMask
-    crgmask_Vi = aMask
-    scmask_Vf = bMask
-    crgmask_Vf = bMask
-    ifsc = '1'
-    ntmin = '2'
-
-  for value in lambdas:
-    CLAMBDA = value
-    IFSC    = ifsc
-    CRGMASK = crgmask_Vi
-    SCMASK  = scmask_Vi
-    NTMIN = ntmin
-    name = '-%s-%s-Vi.in' % (passo,value)
-    writeMin(name,NTMIN, IFSC,CRGMASK,SCMASK,CLAMBDA)
-    writeEqui(name,NTMIN, IFSC,CRGMASK,SCMASK,CLAMBDA)
-    writeProd(name,NTMIN, IFSC,CRGMASK,SCMASK,CLAMBDA)
-
-    IFSC    = ifsc
-    CRGMASK = crgmask_Vf
-    SCMASK  = scmask_Vf
-    name = '-%s-%s-Vf.in' % (passo,value)
-    writeMin(name, NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA)
-    writeEqui(name, NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA)
-    writeProd(name, NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA)
+    groupEqui = """-O -i equi{0}_Vi.in -o equi{0}_Vi.out -p {1} -c min{0}_Vi.rst -r equi{0}_Vi.rst\n"""\
+                """-O -i equi{0}_Vf.in -o equi{0}_Vf.out -p {2} -c min{0}_Vf.rst -r equi{0}_Vf.rst""".format(
+          step_lambda_str, ViPrmtopFile, VfPrmtopFile)
     
-    writeGroup(passo, value,aPrmtopFile,aInpcrdFile,bPrmtopFile,bInpcrdFile)
+    open('equi{0}.groupfile'.format(step_lambda_str), 'w').write(groupEqui)
+    
+    groupProd = """-O -i prod{0}_Vi.in -o prod{0}_Vi.out -p {1} -c equi{0}_Vi.rst -r prod{0}_Vi.rst\n"""\
+                """-O -i prod{0}_Vf.in -o prod{0}_Vf.out -p {2} -c equi{0}_Vf.rst -r prod{0}_Vf.rst""".format(
+          step_lambda_str, ViPrmtopFile, VfPrmtopFile)
+    
+    open('prod{0}.groupfile' .format(step_lambda_str), 'w').write(groupProd)
+  
+
+for step in ['1','2']:
+    if step == '1':
+      scmask_Vi = ''
+      crgmask_Vi = ''
+      scmask_Vf = ''
+      crgmask_Vf = aMask
+      ifsc = '0'
+      ntmin = '1'
+    elif step == '2':
+      scmask_Vi = aMask
+      crgmask_Vi = aMask
+      scmask_Vf = bMask
+      crgmask_Vf = bMask
+      ifsc = '1'
+      ntmin = '2'
+  
+    for lvalue in lambdas:
+      step_lambda_str = '_{0}_{1:.3f}'.format(step, lvalue)
+      CLAMBDA = lvalue
+      IFSC    = ifsc
+      CRGMASK = crgmask_Vi
+      SCMASK  = scmask_Vi
+      NTMIN = ntmin
+      name = '{0}_Vi.in'.format(step_lambda_str)
+      writeMin(name,NTMIN, IFSC,CRGMASK,SCMASK,CLAMBDA)
+      writeEqui(name,NTMIN, IFSC,CRGMASK,SCMASK,CLAMBDA)
+      writeProd(name,NTMIN, IFSC,CRGMASK,SCMASK,CLAMBDA)
+  
+      IFSC    = ifsc
+      CRGMASK = crgmask_Vf
+      SCMASK  = scmask_Vf
+      name = '{0}_Vf.in'.format(step_lambda_str)
+      writeMin(name, NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA)
+      writeEqui(name, NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA)
+      writeProd(name, NTMIN,IFSC,CRGMASK,SCMASK,CLAMBDA)
+      
+      writeGroup(step, lvalue, aPrmtopFile,aInpcrdFile,bPrmtopFile,bInpcrdFile)
