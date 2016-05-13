9aba812925f66978daab0accb25138ff10cf1a14
ze <antoniojosemribeiro@gmail.com>
2016-05-13 09:49:53 +0100

    renamed add_line.py 	renamed:    add_line.py -> gau_add_line.py

--- scripts/gau_add_line.py
+++ scripts/gau_add_line.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
-
+"""Adds a line to gaussian log files to avoid an error in gaussview"""
 
 import sys
 
-
 with open(sys.argv[1], 'r+') as input_file:
     for a in range(1000000):
-        #print input_file.read(10)
         input_file.seek(a)
         if input_file.read(7) == " I=    ":
             break
 
-    input_file.seek(input_file.tell()-7)   
+    input_file.seek(input_file.tell()-7)
     input_file.write("\n I=  ")
     input_file.flush()
 
